e015ddb461e1b6eab1caaccf72b600fe231ec537
Claire W <cdwang@bu.edu>
2016-04-03 05:26:16 -0400

    api

--- public/app.js
+++ public/app.js
@@ -3,8 +3,17 @@
 /* Controllers */
 var tincamApp = angular.module('tincamApp', []);
 
-tincamApp.controller('DropBoxCtrl', function($scope) {
+tincamApp.controller('DropBoxCtrl', function($scope,$http) {
 	$scope.sendToDropBox = function() {
-		// add http request here Claire =D
+		$http({
+			method: 'GET',
+			url: 'https://www.dropbox.com/1/oauth2/authorize?client_id=<YOUR-APP-KEY>&response_type=code',
+			headers: {'Authorization':'Bearer <TOKEN>'},
+			redirect_uri: 'https://www.dropbox.com/*'
+		}).then(function mySuccess(response){
+			console.log(response.data);
+		}, function myError(response){
+			console.log(response.statusText);
+		});
 	};
 });
