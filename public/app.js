39c9f22a4ee30242ef7248b3cf2dc0348ca20d5a
Patrick Wu <scorpio7500@gmail.com>
2016-04-03 05:06:34 -0400

    reorganized project structure

---
+++ public/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* Controllers */
+var tincamApp = angular.module('tincamApp', []);
+
+tincamApp.controller('DropBoxCtrl', function($scope) {
+	$scope.sendToDropBox = function() {
+		// add http request here Claire =D
+	};
+});
